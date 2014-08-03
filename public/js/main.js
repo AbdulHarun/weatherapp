3f695d220a409b8faca3cfde3c5cf5bf42960b15
unknown <abdul.harun@mail.bcu.ac.uk>
2014-08-03 17:06:28 +0100

    + added ajax call to get the information from the api + added a template to show the results + added the success case of the result to hook to the template

--- public/js/main.js
+++ public/js/main.js
@@ -19,6 +19,38 @@ function loadContent(template){
 	$('#searchForm').html(template);
 }
 
+function binds(){
+	$('#searchType').change(function(){
+		var searchTerm = $('#searchTerm');
+		if(this.value == 'ip'){
+			searchTerm.hide();
+		} else {
+			searchTerm.show();
+		}
+	});
+
+	$('#searchSubmit').click(doSearch);
+}
+
+function doSearch(e){
+	var searchType 	= $('#searchType').val();
+	var searchDays 	= $('#searchDays').val();	
+	var searchTerm 	= (searchType == 'ip') ? '' : $('#searchTerm').val().trim();
+	var url 		= apiCalls.search;
+	url 			+= "?searchBy="+searchType+"&searchTerm="+searchTerm+"&searchDays="+searchDays;
+	//should add a loading spinner at this point
+	$.get(url).done(function(data){
+		if(data.cod == 200) successCallback(data)
+	});
+}
+
+function successCallback(data){
+
+	requireTemplate('results',data)
+		.done(function(template){
+			$('#searchResults').html(template);
+		});
+}
 
 
 
