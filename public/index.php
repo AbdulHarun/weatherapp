ca75cc62465e1e2b89bc750be0b22a92f2ebd72b
unknown <abdul.harun@mail.bcu.ac.uk>
2014-08-03 15:45:43 +0100

    +added file basic index file

---
+++ public/index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Weather App</title>
+  </head>
+
+  <body>
+    <div class="header">
+      
+    </div>
+    <div id="content">
+
+    </div>
+
+    <div class="footer">
+
+    </div>
+  </body>
+</html>
