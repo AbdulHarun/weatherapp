a1fbf5b37eb490eb8180f9807551c27b5abbc5b4
unknown <abdul.harun@mail.bcu.ac.uk>
2014-08-03 16:27:36 +0100

    ~updated search to api to now return json ~ will now call the weather class and pass in the argument at point of weather class init
    ~returns excatly what the weather api provides back to the user

--- api/search.php
+++ api/search.php
@@ -4,4 +4,22 @@
 * Title: Weather Application
 * Description: Basic weather application which uses open weather map's api to gather information
 */
+
+//require the class
+require('weather.php');
+
+//we shall be returning it as json so set the header 
+header('Content-Type: application/json');
+
+//define the weather class and add our variables to initialise it
+$weather = new Weather($_GET['searchBy'],$_GET['searchTerm']);
+
+//set the default to current if nothing specified
+$forecastDays = ( isset($_GET['searchDays']) && $_GET['searchDays'] )? $_GET['searchDays'] : 'current';
+
+//call function through curly braces
+$json = $weather->{'search_'.$forecastDays}();
+
+//return the json data back
+echo $json;
 ?>
