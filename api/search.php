2bd80ac29cb4d6e95cf9b1180a79850c07efbe0d
unknown <abdul.harun@mail.bcu.ac.uk>
2014-08-03 15:53:00 +0100

    Note since my playground code is to a good standard, i shall be migrating the code over in chunks. +added search.php which will be used by the client through ajax to access data
    +added the class weather.php which will consist of the code to gather the weather based on users response

---
+++ api/search.php
@@ -0,0 +1,7 @@
+<?php
+/**
+* Author: Abdul Harun
+* Title: Weather Application
+* Description: Basic weather application which uses open weather map's api to gather information
+*/
+?>
---
+++ api/weather.php
@@ -0,0 +1,11 @@
+<?php
+/**
+* Author: Abdul Harun
+* Title: Weather Application
+* Description: Basic weather application which uses open weather map's api to gather information
+*/
+class Weather
+{
+  
+}
+?>
