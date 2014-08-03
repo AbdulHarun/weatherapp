15211a941d12b58c7bdcd398eda26c9c0ece8585
unknown <abdul.harun@mail.bcu.ac.uk>
2014-08-03 23:02:17 +0100

    ~ included moment js

--- public/index.php
+++ public/index.php
@@ -6,12 +6,14 @@
     <title>Weather App</title>
 
     <?php
-      session_start();  
+      //if we decide to have a login session then we can use this but for now its not needed
+      // session_start();  
     ?>
 
     <link href="css/style.css" rel="stylesheet" type="text/css" />
     <script type="text/javascript" src="js/jquery-1.11.1.js"></script>
     <script type="text/javascript" src="js/handlebars-v1.3.0.js"></script>
+    <script type="text/javascript" src="js/moment.js"></script>
     <script type="text/javascript" src="js/main.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=places"></script>
   </head>
@@ -20,6 +22,7 @@
     <div class="header">
       
     </div>
+
     <div id="content">
       <div id="searchForm">
       </div>
